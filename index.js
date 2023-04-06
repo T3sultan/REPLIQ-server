246b8607d8d943f7caaabc9e89f60fd8c347aad1
Tipu Sultan <60041747+T3sultan@users.noreply.github.com>
2023-04-07 03:12:30 +0600

    checkout

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
+
 const port = process.env.PORT || 5000;
 const app = express();
-require("dotenv").config();
-const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const ObjectId = require("mongodb").ObjectId;
+const id = new ObjectId();
+
 const jwt = require("jsonwebtoken");
 
 // ACCESS_TOKEN_SECRET=4e90ca4f3664a3bf8616e78c45eb2c55730058cac3e71a5331484dd1c4000d77a786dbf8b0ef80426aaf02910da366b370dabd3eb6f50ec97a388273d9cebd7d
@@ -23,19 +28,34 @@ async function run() {
   try {
     await client.connect();
     const perliq = client.db("perliq");
-    const parlourCollection = perliq.collection("products");
+    const perliqCollection = perliq.collection("products");
+    const orderCollection = perliq.collection("orders");
 
     //service create api
     app.post("/product", async (req, res) => {
       const newParlour = req.body;
-      const result = await parlourCollection.insertOne(newParlour);
+      const result = await perliqCollection.insertOne(newParlour);
       res.send(result);
     });
     app.get("/product", async (req, res) => {
       const query = {};
-      const cursor = parlourCollection.find(query);
-      const parlour = await cursor.toArray();
-      res.send(parlour);
+      const cursor = perliqCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+    //product details
+    app.get("/product/:id", async (req, res) => {
+      const id = req.params.id;
+
+      const query = { _id: new ObjectId(id) };
+      const product = await perliqCollection.findOne(query);
+      res.send(product);
+    });
+    //     checkout
+    app.post("/order", async (req, res) => {
+      const order = req.body;
+      const result = await orderCollection.insertOne(order);
+      res.send(result);
     });
     //manage service
 
